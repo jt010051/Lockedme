51cb20ef201b5bdd549fa641890e7f3b268cda10
Bas Kuis <b@ukora.com>
2021-01-13 13:39:50 -0600

    add a file

--- lockedme/src/main/java/simplilearn/App.java
+++ lockedme/src/main/java/simplilearn/App.java
@@ -1,6 +1,11 @@
 package simplilearn;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 
 /**
@@ -29,7 +34,7 @@ public class App
                 showFilesInAscendingOrder();
                 break;
             case "2":
-
+                showFileOperations();
             case "3":
                 System.out.println("Thanks for using lockedme.com. Closing application.");
                 System.exit(0);
@@ -38,6 +43,57 @@ public class App
                 System.out.println("Invalid input provided, please choose 1, 2 or 3.");
         }
         showMainMenu();
+    }
+
+    private static void showFileOperations() {
+        System.out.println("--------------");
+        System.out.println("1.) Add a file");
+        System.out.println("2.) Delete a file");
+        System.out.println("3.) Search for a file");
+        System.out.println("4.) Back to main menu");
+        System.out.println("--------------");
+        collectFileOperation();
+    }
+
+    private static void addAFile() throws InvalidPathException {
+        System.out.println("Please provide a file path:");
+        String filePath = scanner.nextLine();
+        Path path = Paths.get(filePath);
+
+        if (!Files.exists(path)) {
+            System.out.println("File does not exist");
+            return;
+        }
+
+        String newFilePath = FOLDER + "/" + path.getFileName();
+        int inc = 0;
+        while (Files.exists(Paths.get(newFilePath))) {
+            inc++;
+            newFilePath = FOLDER + "/" + inc + "_" + path.getFileName();
+        }
+        try {
+            Files.copy(path, Paths.get(newFilePath));
+        } catch(IOException e) {
+            System.out.println("Unable to copy file to " + newFilePath);
+        }
+
+    }
+
+    private static void collectFileOperation() {
+        System.out.println("Please choose 1, 2, 3 or 4:");
+        String option = scanner.nextLine();
+        switch (option) {
+            case "1":
+                addAFile();
+                break;
+            case "2":
+                break;
+            case "3":
+                break;
+            case "4":
+                break;
+        }
+        showFileOperations();
     }
 
     private static void showFilesInAscendingOrder() {
